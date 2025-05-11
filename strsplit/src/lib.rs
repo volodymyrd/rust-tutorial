5c7b24742153928a65b5dd5fa8a07863fbb2a2bf
volodymyrd <volodymyrdotsenko@gmail.com>
2025-05-11 22:59:39 +0200

    fix a bug, use Option for remainder.

--- strsplit/src/lib.rs
+++ strsplit/src/lib.rs
@@ -1,13 +1,13 @@
 #[derive(Debug)]
 pub struct StrSplit<'a> {
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str,
 }
 
 impl<'a> StrSplit<'a> {
     pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter,
         }
     }
@@ -16,16 +16,13 @@ impl<'a> StrSplit<'a> {
 impl<'a> Iterator for StrSplit<'a> {
     type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next_delimiter) = self.remainder.find(self.delimiter) {
-            let until_delimeter = &self.remainder[..next_delimiter];
-            self.remainder = &self.remainder[next_delimiter + self.delimiter.len()..];
+        let remainder = self.remainder.as_mut()?;
+        if let Some(next_delimiter) = remainder.find(self.delimiter) {
+            let until_delimeter = &remainder[..next_delimiter];
+            *remainder = &remainder[(next_delimiter + self.delimiter.len())..];
             Some(until_delimeter)
-        } else if self.remainder.is_empty() {
-            None
         } else {
-            let rest = self.remainder;
-            self.remainder = "";
-            Some(rest)
+            self.remainder.take()
         }
     }
 }
@@ -38,9 +35,13 @@ mod tests {
     fn it_works() {
         let haystack = "a b c d e";
         let splitted = StrSplit::new(haystack, " ");
-        //for e in splitted {
-        //    println!("splitted: {e}");
-        //}
         assert!(splitted.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
+    }
+
+    #[test]
+    fn tail() {
+        let haystack = "a b c d e ";
+        let splitted: Vec<_> = StrSplit::new(haystack, " ").collect();
+        assert_eq!(splitted, vec!["a", "b", "c", "d", "e", ""]);
     }
 }
